c9471dfddc3ebea37efe37e83c02378618cedd5a
m2Link <88938300+m2Link@users.noreply.github.com>
2021-10-17 15:40:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,12 +28,12 @@ async def text(bot, update):
            [
               InlineKeyboardButton('Updates Channel', url='https://t.me/m2botz'),
               InlineKeyboardButton('Support Group', url='https://t.me/m2botzsupport')
-        ],
-        [
+           ],
+           [
               InlineKeyboardButton(text="Search here", switch_inline_query_current_chat=""),
               InlineKeyboardButton(text="Search in another chat", switch_inline_query="")
         ]
-        )
+     )
     await update.reply_text(
         text=text,
         reply_markup=reply_markup,
