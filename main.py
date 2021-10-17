0bc6c7a45a549058b36a6848cecd0c64305dcef5
m2Link <88938300+m2Link@users.noreply.github.com>
2021-10-17 13:26:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,11 +53,11 @@ async def search(bot, update):
         channel_name = result["channel"]["name"]
         channel_link = result["channel"]["link"]
         description = f"{views_short} | {duration}"
-        f"**** {title}" + "\n" "\n" \
-        f"**👑 Channel::** [{channel_name}]({channel_link})" + "\n" "\n" \
-        f"**🕒 Duration:** {duration_text}" + "\n" "\n" \ 
-        f"**👁️‍🗨️ Views:** {views}" + "\n" "\n" \
-        f"**⏰Published Time:** {publishedtime}" + "\n" "\n" \
+        details = f"**Title:** {title}" + "\n" \
+        f"**Channel:** [{channel_name}]({channel_link})" + "\n" \
+        f"**Duration:** {duration_text}" + "\n" \
+        f"**Views:** {views}" + "\n" \
+        f"**Published Time:** {publishedtime}" + "\n" \
         "\n" + "**Made by @FayasNoushad**"
         thumbnail = ytthumb.thumbnail(result["id"])
         reply_markup = InlineKeyboardMarkup(
