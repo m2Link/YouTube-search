be1c4f938ca989051022c806ab0cbfe8a04debb7
m2Link <88938300+m2Link@users.noreply.github.com>
2021-10-17 15:33:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def text(bot, update):
         [
               InlineKeyboardButton(text="Search here", switch_inline_query_current_chat="")],
               InlineKeyboardButton(text="Search in another chat", switch_inline_query="")]
-        ]
+        )
     )
     await update.reply_text(
         text=text,
